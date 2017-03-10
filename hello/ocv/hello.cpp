731865da4c4b369c30076ab46935b6bb6042657b
Hessel van der Molen <hmolen.science@gmail.com>
2017-03-10 16:24:58 +0100

    Added test-sources

---
+++ hello/ocv/hello.cpp
@@ -0,0 +1,32 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, char** argv )
+{
+    if( argc != 2) {
+        cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
+        return -1;
+    }
+        
+    // Read image
+    Mat image;
+    image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    
+    // Check for invalid input
+    if(! image.data ) {
+        cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+
+    // Create a window for display and show our image inside it.
+    namedWindow( "Display window", WINDOW_AUTOSIZE );
+    imshow( "Display window", image );
+    
+    // Wait for a keystroke in the window
+    waitKey(0);
+    return 0;
+}
---
+++ hello/pi/hello.cpp
@@ -0,0 +1,7 @@
+// 'Hello World!' program 
+#include <iostream>
+int main()
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
