24cccc02fdef2e8cd792df5e6cda9673e037ddb4
Hessel van der Molen <hmolen.science@gmail.com>
2017-03-16 14:39:39 +0100

    Update sync-rpi-vm.sh

--- sync-rpi-vm.sh
+++ sync-rpi-vm.sh
@@ -4,6 +4,12 @@
 rsync -auHWv rpizero-local-root:{/usr,/lib} ~/rpi/rootfs
 
 # fix links
-ln -sf /home/pi/rpi/rootfs/lib/arm-linux-gnueabihf/librt.so.1 /home/pi/rpi/rootfs/usr/lib/arm-linux-gnueabihf/librt.so
-ln -sf /home/pi/rpi/rootfs/lib/arm-linux-gnueabihf/libbz2.so.1.0 /home/pi/rpi/rootfs/usr/lib/arm-linux-gnueabihf/libbz2.so
-ln -sf /home/pi/rpi/rootfs/usr/lib/arm-linux-gnueabihf/libpython2.7.so.1.0 /home/pi/rpi/rootfs/usr/lib/arm-linux-gnueabihf/libpython2.7.so
+TARGET_ROOT="/home/pi/rpi/rootfs"
+
+CMD=""
+CMD="$CMD ln -sf $TARGET_ROOT/lib/arm-linux-gnueabihf/librt.so.1 $TARGET_ROOT/usr/lib/arm-linux-gnueabihf/librt.so;"
+CMD="$CMD ln -sf $TARGET_ROOT/lib/arm-linux-gnueabihf/libbz2.so.1.0 $TARGET_ROOT/usr/lib/arm-linux-gnueabihf/libbz2.so;"
+CMD="$CMD ln -sf $TARGET_ROOT/usr/lib/arm-linux-gnueabihf/libpython2.7.so.1.0 $TARGET_ROOT/usr/lib/arm-linux-gnueabihf/libpython2.7.so;"
+
+# fix links in vm
+eval $CMD
