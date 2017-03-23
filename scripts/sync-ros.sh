44a93aeedd7bcb93b0a458968fd10a6ef059d226
HesselM <hmolen.science@gmail.com>
2017-03-23 12:45:56 +0100

    added sync-ros

---
+++ scripts/sync-ros.sh
@@ -0,0 +1,2 @@
+rsync -auHWv --no-perms --no-owner --no-group /home/pi/ros/rpi_cross/devel_isolated rpizero-local:/home/pi/ros/rpi_cross/
+rsync -auHWv --no-perms --no-owner --no-group /home/pi/ros/rpi_cross/src rpizero-local:/home/pi/ros/rpi_cross/
